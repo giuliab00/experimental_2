4210314408c1e1b7d8437c761e8b8ad682959761
Michelangelo [Docker-ROS1] <tabitamarco@yahoo.it>
2023-12-03 08:43:19 +0000

    init

---
+++ src/markerDetector.cpp
@@ -0,0 +1,22 @@
+#include "aruco/aruco.h"
+#include "aruco/cvdrawingutils.h"
+#include "ros/ros.h"
+#include "image_transport/image_transport.h"
+#include "cv_bridge/cv_bridge.h"
+#include "sensor_msgs/image_encodings.h"
+#include "std_msgs/Int32.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Bool.h"
+#include "opencv2/core/mat.hpp"
+#include "nav_msgs/Odometry.h"
+#include <cmath>
+#include <sstream>
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <aruco_ros/aruco_ros_utils.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv){
+	//Ciao
+}
