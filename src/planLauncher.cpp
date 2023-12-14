62058cd3f3f49c9171ac3292a8e887a00e3ad42a
Matteoforni1 <93779567+Matteoforni1@users.noreply.github.com>
2023-12-14 14:47:45 +0000

    Fixed error in order execution and added control on plan dispatch success

--- src/planLauncher.cpp
+++ src/planLauncher.cpp
@@ -24,25 +24,34 @@ class PlanLauncher {
 		bool init_plan() {
 		
 			std_srvs::Empty srv1;
-			rosplan_dispatch_msgs::DispatchService srv2;
-			std_srvs::Empty srv3;
+			std_srvs::Empty srv2;
+			rosplan_dispatch_msgs::DispatchService srv3;
+			
+			bool done = false;
 		
 			// init two services
 			ros::ServiceClient prob_gen = nh_.serviceClient<std_srvs::Empty>("//rosplan_problem_interface/problem_generation_server");
-			ros::ServiceClient plan_disp = nh_.serviceClient<rosplan_dispatch_msgs::DispatchService>("/rosplan_plan_dispatcher/dispatch_plan");
 			ros::ServiceClient prob_solve = nh_.serviceClient<std_srvs::Empty>("/rosplan_planner_interface/planning_server");
+			ros::ServiceClient plan_disp = nh_.serviceClient<rosplan_dispatch_msgs::DispatchService>("/rosplan_plan_dispatcher/dispatch_plan");
 			
-			// call the two services to start the plan
-			prob_gen.call(srv1);
+			// TODO: CHANGE THE CONDITION OF THE WHILE WITH A VARIABLE AMONG done AND goal
+			while (not done) {
 			
-			plan_disp.call(srv2);
+				// call the two services to start the plan
+				prob_gen.call(srv1);
 			
-			prob_solve.call(srv3);
+				prob_solve.call(srv2);
 			
-			// get info about if we succeeded or failed
-			bool done = srv2.response.success;
-			bool goal = srv2.response.goal_achieved;
+				plan_disp.call(srv3);
 			
+				// get info about if we succeeded or failed
+				done = srv3.response.success;
+				bool goal = srv3.response.goal_achieved;
+				
+				ROS_INFO("My boolean value: %s", done ? "true" : "false");
+				ROS_INFO("My boolean value: %s", goal ? "true" : "false");
+			
+			}
 		}
 	
 };
